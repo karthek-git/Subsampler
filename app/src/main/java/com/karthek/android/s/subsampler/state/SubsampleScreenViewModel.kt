3667fe0d12c4cef9ccd2fe54f33867192e42051c
Karthik Alapati <mail@karthek.com>
2023-02-16 23:14:24 +0530

    add NAC

--- app/src/main/java/com/karthek/android/s/subsampler/state/SubsampleScreenViewModel.kt
+++ app/src/main/java/com/karthek/android/s/subsampler/state/SubsampleScreenViewModel.kt
@@ -1,19 +1,21 @@
 package com.karthek.android.s.subsampler.state
 
+import android.app.Application
 import android.net.Uri
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import com.karthek.android.s.subsampler.subSample
+import com.karthek.android.s.subsampler.NAC
+import com.karthek.android.s.subsampler.ml.model.FaceDetector
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
 import java.io.FileInputStream
 
-class SubsampleScreenViewModel : ViewModel() {
+class SubsampleScreenViewModel(appContext: Application) : AndroidViewModel(appContext) {
 	var imageUri by mutableStateOf<Uri?>(null)
 	var samplingInProgress by mutableStateOf(false)
 	var showSaved by mutableStateOf(false)
@@ -24,12 +26,14 @@ class SubsampleScreenViewModel : ViewModel() {
 	var gotSize by mutableStateOf(0L)
 	val outputStream by lazy { ByteArrayOutputStream() }
 
+	private val nac by lazy { NAC(FaceDetector(appContext)) }
+
 	fun runForSize() {
 		viewModelScope.launch {
 			samplingInProgress = true
 			gotSize = withContext(Dispatchers.Default) {
 				inputStream?.let {
-					subSample(origSize, reqSize * 1024, it, outputStream)
+					nac.compress(origSize, reqSize * 1024, it, outputStream)
 				} ?: 0
 			}
 			samplingInProgress = false
